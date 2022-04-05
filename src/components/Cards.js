0acbae86aae1248e24b1cd22b550854f66e6fe7f
rhazbrown <rhazbrown@gmail.com>
2022-04-06 00:53:53 +0300

    update to herosec css and html bg-color black

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import CardItem from './CardItem';
-import './Cards.css';
-
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
 
 function Cards() {
   return (
-    <div className='cards'>
+    <div className="cards">
       <h1>SERVICES</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <CardItem
-              src='images/education.jpg'
-              text='Our agency pairs you with the best affordable schools in Europe and Canada. Germany, Norway, South and North Cyprus 50-100%'
-              label='Study Abroad'
-              path='/study-abroad'
+              src="images/education.jpg"
+              text="Our agency pairs you with the best affordable schools in Europe and Canada. Germany, Norway, South and North Cyprus 50-100%"
+              label="Study Abroad"
+              path="/study-abroad"
             />
-             <CardItem
-              src='images/graphic.png'
-              text='Providing High-quality Custom Graphic Designs For an Extremely Affordable Price. Try it! Extremely Affordable, Fast Delivery (1-3 days), Customized for You. 14D Money-back Guarantee.'
-              label='Graphic Designs'
-              path='/services'
+            <CardItem
+              src="images/graphic.png"
+              text="Providing High-quality Custom Graphic Designs For an Extremely Affordable Price. Try it! Extremely Affordable, Fast Delivery (1-3 days), Customized for You. 14D Money-back Guarantee."
+              label="Graphic Designs"
+              path="/services"
             />
-            
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <CardItem
-              src='images/web.png'
-              text='Trust our team with over 4 years experience. Developers, Designers, UX Experts and Marketers. Fast Delivery, Eco Packages from $800, Premium Photos, affordable Hosting and Support.'
-              label='Web development'
-              path='/study-abroad'
+              src="images/web.png"
+              text="Trust our team with over 4 years experience. Developers, Designers, UX Experts and Marketers. Fast Delivery, Eco Packages from $800, Premium Photos, affordable Hosting and Support."
+              label="Web development"
+              path="/study-abroad"
             />
-             <CardItem
-              src='images/writting.png'
-              text='Providing you with all types of high-quality writting from, thesis writting, movie scripts etc. Tailored to meet your need.'
-              label='All types of writting'
-              path='/services'
+            <CardItem
+              src="images/writting.png"
+              text="Providing you with all types of high-quality writting from, thesis writting, movie scripts etc. Tailored to meet your need."
+              label="All types of writting"
+              path="/services"
             />
-            
           </ul>
         </div>
       </div>
