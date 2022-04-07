4aff6a23a64a950387e848d7f5b4cc44da21aa57
rhazbrown <rhazbrown@gmail.com>
2022-04-07 17:45:13 +0300

    added trailler video and trailler button function

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typewriter } from "react-simple-typewriter";
 import { Button } from "./Button";
 import "./HeroSection.css";
 //import { Link } from "react-router-dom";
 import { Link, animateScroll as scroll } from "react-scroll";
+import { IoCloseOutline } from "react-icons/io5";
+import { BiLoaderAlt } from "react-icons/bi";
 
 function HeroSection() {
+  const [modal, setModal] = useState(false);
+  const [videoLoading, setVideoLoading] = useState(true);
+
+  const openModal = () => {
+    setModal(!modal);
+  };
+
+  const spinner = () => {
+    setVideoLoading(!videoLoading);
+  };
   return (
     <div className="hero-container" id="home">
       <video
@@ -28,7 +40,7 @@ function HeroSection() {
                 "Website Development!",
                 "All Types Of Writting!",
               ]}
-              loop={Infinity}
+              loop={0}
               cursor
               cursorStyle="|"
               typeSpeed={150}
@@ -58,11 +70,47 @@ function HeroSection() {
           </Button>
         </Link>
         <Button
+          onClick={openModal}
           className="btns"
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
           WATCH TRAILER <i className="far fa-play-circle" />
+          {modal ? (
+            <section className="modal__bg">
+              <div className="modal__align">
+                <div className="modal__content" modal={modal}>
+                  <IoCloseOutline
+                    className="modal__close"
+                    arial-label="Close modal"
+                    onClick={setModal}
+                  />
+                  <div className="modal__video-align">
+                    {videoLoading ? (
+                      <div className="modal__spinner">
+                        <BiLoaderAlt
+                          className="modal__spinner-style"
+                          fadeIn="none"
+                        />
+                      </div>
+                    ) : null}
+                    <iframe
+                      className="modal__video-style"
+                      onLoad={spinner}
+                      loading="lazy"
+                      width="800"
+                      height="500"
+                      src="https://res.cloudinary.com/wprodigy/video/upload/q_38/v1649340354/Assets/VID-20220121-WA0007_dlqsgg.mp4"
+                      title="video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </div>
+              </div>
+            </section>
+          ) : null}
         </Button>
       </div>
     </div>
