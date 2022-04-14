525d694e2676d33cef769445436595a033bb4f25
Edgar John <45301854+ebinabo@users.noreply.github.com>
2022-04-14 17:05:29 +0300

    Hide Navbar on Sign Up page (#2)
    * hide navbar on sign up page
    
    * remove console log

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import Home from "./components/pages/Home";
+import { AccountBox } from "./components/accountBox/signup";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import About from "./components/pages/about";
 import ContactUs from "./components/pages/contact";
 import Design from "./components/pages/design";
+import Home from "./components/pages/Home";
 import Pricing from "./components/pages/pricing";
 import Study from "./components/pages/study";
-import Footer from "./components/Footer";
-import { AccountBox } from "./components/accountBox/signup";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/Sign-Up" element={<AccountBox />} />
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Contact-Us" element={<ContactUs />} />
-          <Route path="/Get-a-design" element={<Design />} />
-          <Route path="/Pricing" element={<Pricing />} />
-          <Route path="/Study-Abroad" element={<Study />} />
-        </Routes>
-        <Footer />
-      </Router>
+      {pathname === "/Sign-Up" ? null : <Navbar />}
+      <Routes>
+        <Route path="/Sign-Up" element={<AccountBox />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Contact-Us" element={<ContactUs />} />
+        <Route path="/Get-a-design" element={<Design />} />
+        <Route path="/Pricing" element={<Pricing />} />
+        <Route path="/Study-Abroad" element={<Study />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
